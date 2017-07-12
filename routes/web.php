907d56828b78b586d474d6eca5c196cd9a2ac9fd
Rizwan Aslam(Am) <rizwan@codebrisk.com>
2017-07-13 01:46:21 +0500

    dashboard route make

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('admin/dashboard');
 });
 
+Route::get('/dashboard', array(
+    'as' => 'get.dashboard',
+    'uses' => 'Admin\AdminController@dashboard'));
+
 
 Auth::routes();
 
