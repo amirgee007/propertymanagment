0cfa1c5b7e9f97c5fcd17ccad2e92c019792daae
Rizwan Aslam(Am) <rizwan@codebrisk.com>
2017-07-14 02:04:05 +0500

    ok

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\MessageBag;
 
 class LoginController extends Controller
 {
@@ -25,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
@@ -35,5 +38,33 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    public function showLoginForm()
+    {
+        if (Auth::user()) {
+            return redirect('/dashboard');
+        }
+        $errors = new MessageBag(['password' => ['Email and/or password invalid or Not Admin.']]);
+        return view('admin.auths.login')->withErrors($errors)->withInput(Input::except('password'));;
+
+    }
+
+    public function logout(){
+
+        Auth::logout();
+        return redirect('/login');
+
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
     }
 }
