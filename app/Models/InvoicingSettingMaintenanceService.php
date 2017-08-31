a0674f63f18ff035190c7a4af4f2708ca4f69741
Rizwan Aslam <rizwan@codebrisk.com>
2017-09-01 00:10:07 +0500

    make service to add settings globally in the app

--- app/Models/InvoicingSettingMaintenanceService.php
+++ app/Models/InvoicingSettingMaintenanceService.php
@@ -8,4 +8,9 @@ class InvoicingSettingMaintenanceService extends Model
 {
     protected $table = 'invoicing_setting_maintenance_service';
     protected $guarded= [];
+
+    public function scopeByUser($query, $user_id)
+    {
+        return $query->where('user_id', $user_id);
+    }
 }
---
+++ app/Providers/SettingServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\InvoicingSettingGeneral;
+use App\Models\InvoicingSettingMaintenanceService;
+use App\Models\InvoicingSettingUtility;
+use App\Models\SystemSetting;
+use Carbon\Carbon;
+use Illuminate\Support\ServiceProvider;
+
+class SettingServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $system_setting = SystemSetting::first();
+        $invoice_setting = InvoicingSettingGeneral::first();
+        $setting_maintenance = InvoicingSettingMaintenanceService::first();
+        $setting_utility = InvoicingSettingUtility::first();
+
+        config([
+            'tax' => @$system_setting->tax == 1 ? true : false,
+            'interest' => @$system_setting->interest == 1 ? true : false,
+            'client_access' => @$system_setting->client_access == 1 ? true : false,
+            'utility_module' => @$system_setting->utility_module == 1 ? true : false,
+            'sinking_funds_module' => @$system_setting->sinking_funds_module == 1 ? true : false,
+            'car_parks_module' => @$system_setting->car_parks_module == 1 ? true : false,
+
+            'invoice_creation_day' => @$invoice_setting->invoice_creation_day ? $invoice_setting->invoice_creation_day : 0,
+            'invoice_due_date' => @$invoice_setting->invoice_due_date ? Carbon::today()->addDays($invoice_setting->invoice_due_date) : null,
+            'interest_rate' => @$invoice_setting->interest_rate ? $invoice_setting->interest_rate : null,
+
+            'invoice_repeat_month' => @$setting_maintenance->invoice_repeat_month ? $setting_maintenance->invoice_repeat_month : null,
+            'fee_charged' => @$setting_maintenance->fee_charged ? $setting_maintenance->fee_charged : null,
+            'charges_per_sqft' => @$setting_maintenance->charges_per_sqft ? $setting_maintenance->charges_per_sqft : null,
+            'billing_ending' => @$setting_maintenance->billing_ending ? $setting_maintenance->billing_ending : null,
+            'tax_type' => @$setting_maintenance->tax_type ? $setting_maintenance->tax_type : null,
+
+            'utility_invoice_repeat_month' => @$setting_utility->invoice_repeat_month ? $setting_utility->invoice_repeat_month : null,
+            'billing_start_date' => @$setting_utility->billing_start_date ? $setting_utility->billing_start_date : null,
+            'utility_billing_ending' => @$setting_utility->billing_ending ? $setting_utility->billing_ending : null,
+            'utility_tax_type' => @$setting_utility->tax_type ? $setting_utility->tax_type : null,
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
