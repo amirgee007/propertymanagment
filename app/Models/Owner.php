3adf388e20c774291af7290f422f4c086d75ed2b
Rizwan Aslam <rizwan@codebrisk.com>
2017-10-10 14:53:37 +0500

    fix

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -11,19 +11,7 @@ class Owner extends Model
     protected $table = 'owners';
     protected $primaryKey = 'owner_id';
 
-    protected $fillable = [
-        'owner_id',
-        'user_id',
-        'owner_type',
-        'owner_id_card_no',
-        'owner_name',
-        'owner_dob',
-        'owner_gender',
-        'owner_address',
-        'owner_phone1',
-        'owner_phone2',
-        'email'
-    ];
+    protected $guarded = [];
 
     protected $appends = ['status'];
 
